3660dac54bae0bd2063def50086c8526b2435848
Eystein Bye <eysteinbye@gmail.com>
2012-09-04 20:48:22 +0200

    Made WS return JSON

--- TvGuide/Program.asmx.vb
+++ TvGuide/Program.asmx.vb
@@ -1,41 +1,47 @@
 ﻿Imports System.Web.Services
 Imports System.Web.Services.Protocols
 Imports System.ComponentModel
+Imports System.Web.Script.Services
 
-' To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
 <System.Web.Script.Services.ScriptService()> _
-<System.Web.Services.WebService(Namespace:="http://tempuri.org/")> _
+<System.Web.Services.WebService(Namespace:="http://tv-guide.apphb.com/")> _
 <System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
 <ToolboxItem(False)> _
 Public Class Program
     Inherits System.Web.Services.WebService
 
+    Private serialize As New Script.Serialization.JavaScriptSerializer()
+
     <WebMethod()> _
-    Public Function GetAllChanels() As Array
+    <ScriptMethod(ResponseFormat:=ResponseFormat.Json)>
+    Public Function GetAllChanels() As String
         Dim channelList As New BEO.ChannelList
         'Dim list As List(Of KeyValuePair(Of String, String)) = list.List.ToList
-        Return channelList.List.ToArray
+        Return serialize.Serialize(channelList.List.ToArray)
     End Function
 
     <WebMethod()> _
-    Public Function GetInfoForChannel(channelId As String) As BEO.ChannelInfo
-        Return New BEO.ChannelInfo(channelId)
+    <ScriptMethod(ResponseFormat:=ResponseFormat.Json)>
+    Public Function GetInfoForChannel(channelId As String) As String
+        Return serialize.Serialize(New BEO.ChannelInfo(channelId))
     End Function
 
     <WebMethod()> _
-    Public Function GetProgramForChannel(channelName As String, programDate As Date) As List(Of BEO.Program)
+    <ScriptMethod(ResponseFormat:=ResponseFormat.Json)>
+    Public Function GetProgramForChannel(channelName As String, programDate As Date) As String
         Dim ch As New BEO.Channel(channelName, programDate)
-        Return ch.Programs
+        Return serialize.Serialize(ch.Programs)
     End Function
 
     <WebMethod()> _
-    Public Function GetTodaysProgramForChannels(channelNames As List(Of String)) As List(Of BEO.Program)
+    <ScriptMethod(ResponseFormat:=ResponseFormat.Json)>
+    Public Function GetTodaysProgramForChannels(channelNames As List(Of String)) As String
         Dim result As New List(Of BEO.Program)
         For Each channel In channelNames
             Dim ch As New BEO.Channel(channel, DateTime.Today)
             result.AddRange(ch.Programs)
         Next
-        Return result
+        Return serialize.Serialize(result)
     End Function
 
 End Class
